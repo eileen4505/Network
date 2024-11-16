b891d4a55b9f8051b4b51386ae7d067ad58a5ce5
Saeryeam Park <eileen4505@gachon.ac.kr>
2024-11-16 17:55:09 +0900

    2nd commit. If you send incorrect answer, notice correct answer. And showing final score.

--- Server.java
+++ Server.java
@@ -46,10 +46,10 @@ public class Server {
                         out.println("Correct!");
                         score++;
                     } else {
-                        out.println("Incorrect!");
+                        out.println("Incorrect! The correct answer is " + qa[1]);
                     }
                 }
-                out.println("Quiz Over! ");
+                out.println("Quiz Over! The final score is " + score);
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
